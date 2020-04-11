0d016b837648db38d0e6509842ed582537329463
sudhuvrao <sudarshanvrao@gmail.com>
2020-04-12 00:23:11 +0530

    Updates

---
+++ precache-manifest.4d48c030bffbf0b776f0cadbe238a006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141d66d11f0b1120f341283464c74e00",
+    "url": "/8-tiles-game/index.html"
+  },
+  {
+    "revision": "18db88812d1cdd067f6b",
+    "url": "/8-tiles-game/static/css/main.eb227582.chunk.css"
+  },
+  {
+    "revision": "6b162a7aecfaf3057cef",
+    "url": "/8-tiles-game/static/js/2.d3b7e3c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/8-tiles-game/static/js/2.d3b7e3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18db88812d1cdd067f6b",
+    "url": "/8-tiles-game/static/js/main.af71d27c.chunk.js"
+  },
+  {
+    "revision": "e13c45ee4f10b9a89434",
+    "url": "/8-tiles-game/static/js/runtime-main.44f41d3b.js"
+  }
+]);
